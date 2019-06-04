dc56fb229607372e71a6bf2fbd08115b7bf612cd
Neil Pankey <npankey@gmail.com>
2019-06-04 12:38:53 -0500

    Package-ify mtx decoding

--- sfnt/mtx/mtx.go
+++ sfnt/mtx/mtx.go
@@ -1,4 +1,4 @@
-package main
+package mtx
 
 /*
 #cgo LDFLAGS: -L${SRCDIR}/lzcomp -llzcomp
@@ -8,46 +8,39 @@ package main
 */
 import "C"
 import (
-	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
 	"unsafe"
 )
 
-func main() {
-	f, err := os.Open(os.Args[1])
+func DecodeCTF(r io.Reader) ([]byte, error) {
+	buf, err := ioutil.ReadAll(r)
 	if err != nil {
-		panic(err)
-	}
-	buf, err := ioutil.ReadAll(f)
-	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
     var mtx *C.mtx_t
 	if !C.mtx_init(&mtx, (*C.uchar)(unsafe.Pointer(&buf[0])), C.ulong(len(buf))) {
-		panic("Failed to init MTX struct")
+		return nil, fmt.Errorf("Failed to init MTX struct")
 	}
 	defer C.mtx_fini(mtx)
-
-	//C.mtx_dump(mtx)
 
 	var rest, data, code *C.uchar
 	var restSize, dataSize, codeSize C.ulong
 
 	if !C.mtx_getRest(mtx, &rest, &restSize) {
-		panic("Failed to decompress rest block")
+		return nil, fmt.Errorf("Failed to decompress rest block")
 	}
 	defer C.free(unsafe.Pointer(rest))
 
 	if !C.mtx_getData(mtx, &data, &dataSize) {
-		panic("Failed to decompress data block")
+		return nil, fmt.Errorf("Failed to decompress data block")
 	}
 	defer C.free(unsafe.Pointer(code))
 
 	if !C.mtx_getCode(mtx, &code, &codeSize) {
-		panic("Failed to decompress code block")
+		return nil, fmt.Errorf("Failed to decompress code block")
 	}
 	defer C.free(unsafe.Pointer(data))
 
@@ -56,5 +49,5 @@ func main() {
 	ctf = append(ctf, C.GoBytes(unsafe.Pointer(data), C.int(dataSize))...)
 	ctf = append(ctf, C.GoBytes(unsafe.Pointer(code), C.int(codeSize))...)
 
-	io.Copy(os.Stdout, bytes.NewReader(ctf))
+	return ctf, nil
 }
